a25ef9eeb757b8a5780d83015966d0b16222407f
Nick Valdez <nicholastvaldez@gmail.com>
2023-08-23 10:50:06 -0500

    add ask function

--- Program.cs
+++ Program.cs
@@ -39,3 +39,28 @@ void MooseSays(string message)
 
 MooseSays("H I, I'M  E N T H U S I A S T I C !");
 MooseSays("I really am enthusiastic!");
+
+bool MooseAsks(string question)
+{
+    Console.Write($"{question} (Y/N): ");
+    string answer = Console.ReadLine().ToLower();
+
+    while (answer != "y" && answer != "n")
+    {
+        Console.Write($"{question} (Y/N): ");
+        answer = Console.ReadLine().ToLower();
+    }
+
+    if (answer == "y")
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+
+}
+
+bool isTrue = MooseAsks("Is Canada real?");
+Console.WriteLine(isTrue);
